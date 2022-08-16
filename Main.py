c66382a46d51264949f4a7dff16466be9dbda7c8
manoahnoble <110010556+manoahnoble@users.noreply.github.com>
2022-08-16 12:42:02 +0530

    Second Commit

--- Main.py
+++ Main.py
@@ -2,65 +2,65 @@ from typing import Optional
 
 
 class Node:
+    def _init_(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+from typing import Optional
+
+class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
 
 class LinkedList:
-  
     def __init__(self):
         self.head = None
 
     def insert_at_end(self, data):
-        """
-        Insert node at end of the list
-        :param data: integer data that will be used to create a node
-        """
-        newNode = Node(data)
-        if(self.head):
-            current = self.head
-            while(current.next):
-                current = current.next
-                current.next = newNode
+        new = Node(data, None)
+        current = self.head
+        if current is None:
+            self.head = new
         else:
-            self.head = newNode
+            while current.next is not None:
+                current = current.next
+            current.next = new
 
     def status(self):
-        """
-        It prints all the elements of list.
-        """
-        for i in self.next:
-            print(self.data)
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
 
 
 class Solution:
-    """
-    Provide necessary documentation
-    """
-    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
-        """
-        :param first_list: Linkedlist with non-negative integers
-        :param second_list: Linkedlist with non-negative integers
-        :return: returns the sum as a linked list
-        """
-        s=""
-        s1=""
-        for i in first_list:
-            s=str(i)+s
-        for j in second_list:
-            s1=str(j)+s1
-        a=int(s)
-        b=int(s1)
-        c=a+b
-        return c
-        
-        
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+        LinkedList]:
+        result = self.get_num(first_list) + self.get_num(second_list)
+        sum_list = LinkedList()
+        for digit in list(map(int, str(result)[::-1])):
+            sum_list.insert_at_end(digit)
+        return sum_list
 
-# Do not edit the following code      
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        curr = l.head
+        if curr is None:
+            return 0
+        num = ""
+        while curr is not None:
+            num = str(curr.data) + num
+            curr = curr.next
+        return int(num)
+
+
+# Do not edit the following code
 # Create an instance for LinkedList
 first_list = LinkedList()
-# Create an another instance for LinkedList
+# Create an another instance for LinkedListT
 second_list = LinkedList()
 # Read data for first list
 data_for_first_list = list(map(int, input().strip().split(" ")))
@@ -76,5 +76,7 @@ for data in data_for_second_list:
 solution = Solution()
 # Pass first_list and second_list to addTwoNumbers, which returns a new linked list
 new_list = solution.addTwoNumbers(first_list, second_list)
+# Display the status of new_list
+new_list.status()
 # Display the status of new_list
 new_list.status()
